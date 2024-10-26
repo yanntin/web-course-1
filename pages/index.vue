d90024efdf2f9fb35a9f1455f072b8badd8c805a
xinyu.yann <xinyu.yan@gmail.com>
2024-10-26 15:40:32 +0800

    feat: init project

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+  <template>
+    <div class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; width: 100vw; background-image: linear-gradient(to bottom right, #007bff, #ffd700); position: absolute; top: 0; left: 0;">
+      <h1 style="word-break: break-all; margin-bottom: 20px; font-weight: bold; font-size: 36px;">独立开发者 yann正式出发</h1>
+      <p style="word-break: break-all; margin-bottom: 40px;">我们正在开发一系列有趣的工具，敬请期待！</p>
+      <UButton style="border: 1px solid #333; background-color: transparent; color: #333;">立即订阅</UButton>
+    </div>
+  </template>
+  
+  <script setup></script>
