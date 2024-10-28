0d894a8464fa813b8449e96cfb8020a2f4743b41
yanxinyu <yanxinyu01@corp.netease.com>
2024-10-28 21:48:25 +0800

    feat: beautify the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
   <template>
-    <div class="container" style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh; width: 100vw; background-image: linear-gradient(to bottom right, #007bff, #ffd700); position: absolute; top: 0; left: 0;">
-      <h1 style="word-break: break-all; margin-bottom: 20px; font-weight: bold; font-size: 36px;">独立开发者 yann正式出发</h1>
-      <p style="word-break: break-all; margin-bottom: 40px;">我们正在开发一系列有趣的工具，敬请期待！</p>
-      <UButton style="border: 1px solid #333; background-color: transparent; color: #333;">立即订阅</UButton>
+    <div class="w-full px-4 flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 via-purple-50 to-amber-100">
+      <h1 class="text-4xl font-bold">独立开发者 yann正式出发 🛫</h1>
+      <p class="text-gray-500 my-8">我们正在开发一系列有趣的工具，敬请期待！</p>
+      <p class="mb-12">
+        <img src="/image001.jpeg" alt="yann" class="w-auto h-64 rounded-lg">
+      </p>
+      <UButton 
+        to="https://www.bing.com" 
+        target="_blank" 
+        variant="outline" 
+        class="w-48 hover:bg-gray-100 flex items-center justify-center">立即关注 👍</UButton>
     </div>
   </template>
-  
   <script setup></script>
